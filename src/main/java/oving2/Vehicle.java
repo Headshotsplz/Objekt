a8b5d319e8d4fd73246af127cd634b5eb65aa136
Markus Mikalsen <markusandresen11@gmail.com>
2025-04-06 11:38:27 +0200

    Test

---
+++ src/main/java/oving2/Vehicle.java
@@ -0,0 +1,27 @@
+package oving2;
+
+public class Vehicle {
+
+    private char fuelType;
+    private char vehicleType;
+    private int RegistrationNumber;
+    
+    
+
+    public Vehicle(char FuelType, char vechicleType, String RegistrationNumber){
+
+
+    }
+    
+    public char getFuelType(){
+        
+    }
+    public String getRegistrationNumber(){
+
+    }
+    public char getVehicleType(){
+
+    }
+
+    
+}
--- src/test/java/oving2/VehicleTest.java
+++ src/test/java/oving2/VehicleTest.java
@@ -1,4 +1,4 @@
-package oving2;
+xpackage oving2;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
